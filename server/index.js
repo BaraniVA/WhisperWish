85f66086d67417bcf6db237e0685f7c1861e57f1
BaraniVA <baranianandakumar2003@gmail.com>
2025-03-03 16:54:57 +0530

    updates

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/wishes', wishesRouter);
+app.use('/wishes', wishesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
